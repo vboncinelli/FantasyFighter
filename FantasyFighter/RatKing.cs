8650864210ef3f0688e4afe8b0100851af7eea77
Vanni Boncinelli <vanni.boncinelli@remira.com>
2023-12-13 12:10:45 +0100

    add new character (RatKing)

---
+++ FantasyFighter/RatKing.cs
@@ -0,0 +1,19 @@
+﻿namespace FantasyFighter
+{
+    public class RatKing : Character, ICanAttack, ICanDefend
+    {
+        public RatKing(string name, int health, int attackPoints, int defensePoints) : base(name, health, attackPoints, defensePoints)
+        {
+        }
+
+        public void Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Defend()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
